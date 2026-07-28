92962c3d97ef516b99466844033241266e59f5a4
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-28 14:15:19 -0400

    Create IMarketPriceProvider.cs

---
+++ StockPortfolioSimulator/MarketData/IMarketPriceProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace StockPortfolioSimulator.MarketData;
+
+public interface IMarketPriceProvider
+{
+    decimal GetCurrentPrice(Asset asset);
+}
+
+// Just so this will compile until you have your own Asset Model
+// TODO: Delete this class
+public sealed class Asset
+{
+    string Symbol = string.Empty;
+    string Name = string.Empty;
+}
