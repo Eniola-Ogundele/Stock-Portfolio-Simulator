88ee1c1ccaff6d49225674ff7bd869435fefa12f
Eniola Ogundele <rooseveltogundele7@gmail.com>
2026-08-08 03:43:59 -0400

    Protect Holding invariants

--- StockPortfolioSimulator/Models/Holding.cs
+++ StockPortfolioSimulator/Models/Holding.cs
@@ -21,6 +21,16 @@ public class Holding
 
     public Holding(Asset asset, decimal quantity, decimal averagePurchasePrice)
     {
+        if (quantity <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(quantity));
+        }
+
+        if (averagePurchasePrice <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(averagePurchasePrice));
+        }
+
         Asset = asset;
         Quantity = quantity;
         AveragePurchasePrice = averagePurchasePrice;
@@ -28,6 +38,16 @@ public class Holding
 
     public void AddPurchase(decimal quantity, decimal purchasePrice)
     {
+        if(quantity <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(quantity));
+        }
+
+        if(purchasePrice <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(purchasePrice));
+        }
+
         decimal existingCost = Quantity * AveragePurchasePrice;
         decimal newCost = quantity * purchasePrice;
         decimal totalQuantity = quantity + Quantity;
@@ -37,6 +57,16 @@ public class Holding
 
     public void RemoveQuantity(decimal quantity)
     {
+        if(quantity <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(quantity));
+        }
+
+        if(quantity > Quantity)
+        {
+            throw new InvalidOperationException("Not enough shares...");
+        }
+
         Quantity -= quantity;
     }
 
